945a0bce323c17c61784874ad0a0f710cb6fe6f4
Morgan Hayes <jmorganh@users.noreply.github.com>
2016-10-12 16:12:06 -0600

    Create navbar-close.js

---
+++ navbar-close.js
@@ -0,0 +1,16 @@
+   
+   'close navbar on nav click
+    $(document).on('click','.navbar-collapse.in',function(e) {
+        if( $(e.target).is('a') ) {
+            $(this).collapse('hide');
+        }
+    });
+    
+    'close navbar when clicking anywhere else
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
+    });
