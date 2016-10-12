c572d93e8d886531c834970dd39324d5312eef15
Morgan Hayes <jmorganh@users.noreply.github.com>
2016-10-12 16:15:51 -0600

    Create filters.js

---
+++ filters.js
@@ -0,0 +1,6 @@
+'capitalize first letter
+.filter('capitalize', function() {
+       return function(input) {
+           return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+       }
+    });
